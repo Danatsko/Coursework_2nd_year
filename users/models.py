8fceca9c23b8696eaa92892fc64354441f263dc7
Danatsko <oleksandrdanatsko@gmail.com>
2024-04-22 22:24:49 +0300

    Створив початковий сервер Django.

---
+++ users/models.py
@@ -0,0 +1,9 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    patronymic = models.CharField(verbose_name='Patronymic', max_length=150, blank=True)
+    sex = models.CharField(verbose_name='Sex', max_length=20, blank=True)
+    date_of_birth = models.DateField(verbose_name='Date of birth', blank=True)
+    phone_number = models.CharField(verbose_name='Phone number', max_length=20, blank=True)
