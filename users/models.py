28543067a6c87eee6dda546e9e1c7b06a1634280
Danatsko <oleksandrdanatsko@gmail.com>
2024-04-24 22:19:26 +0300

    Створив функцію для авторизації користувача.

--- users/models.py
+++ users/models.py
@@ -1,3 +1,6 @@
+import datetime
+
+import django.utils.timezone
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
@@ -5,5 +8,5 @@ from django.db import models
 class User(AbstractUser):
     patronymic = models.CharField(verbose_name='Patronymic', max_length=150, blank=True)
     sex = models.CharField(verbose_name='Sex', max_length=20, blank=True)
-    date_of_birth = models.DateField(verbose_name='Date of birth', blank=True)
+    date_of_birth = models.DateField(verbose_name='Date of birth', blank=True, default=datetime.datetime(2005, 10, 12))
     phone_number = models.CharField(verbose_name='Phone number', max_length=20, blank=True)
--- users/views.py
+++ users/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.contrib.auth import authenticate, login, get_user_model
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+
+
+@csrf_exempt
+def user_login(request):
+    if request.user.is_authenticated == False:
+        if request.method == 'POST':
+            parameters = dict(json.loads(request.body))
+
+            if (('username' in parameters) and
+                    ('password' in parameters) and
+                    (len(parameters) == 2)
+            ):
+                if get_user_model().objects.filter(username=parameters['username']).exists():
+                    user_authentication = authenticate(request,
+                                                       username=parameters['username'],
+                                                       password=parameters['password'],
+                                                       )
+
+                    if user_authentication:
+                        login(request, user_authentication)
+
+                        answer = {'Status': 'Success',
+                                  'Massage': 'Login.'
+                                  }
+                    else:
+                        answer = {'Status': 'Fail',
+                                  'Massage': 'Wrong password.'
+                                  }
+                else:
+                    answer = {'Status': 'Fail',
+                              'Massage': 'User does not exist.'
+                              }
+            else:
+                answer = {'Status': 'Fail',
+                          'Massage': 'Wrong parameters.'
+                          }
+        else:
+            answer = {'Status': 'Fail',
+                      'Massage': 'Wrong method.'
+                      }
+    else:
+        answer = {'Status': 'Fail',
+                  'Massage': 'Authenticated.'
+                  }
+
+    return JsonResponse(answer)
