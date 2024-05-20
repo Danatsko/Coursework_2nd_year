1294f21746cca0d773289908515f227586089973
Danatsko <oleksandrdanatsko@gmail.com>
2024-05-20 23:39:55 +0300

    Створив таблицю "Координати таксистів".

--- users/models.py
+++ users/models.py
@@ -9,3 +9,15 @@ class User(AbstractUser):
     sex = models.CharField(verbose_name='Sex', max_length=20, blank=True)
     date_of_birth = models.DateField(verbose_name='Date of birth', blank=True, default=datetime.datetime(2005, 10, 12))
     phone_number = models.CharField(verbose_name='Phone number', max_length=20, blank=True)
+
+class TaxiDriversCoordinates(models.Model):
+    taxi_driver_id = models.CharField(verbose_name='ID таксиста', max_length=250)
+    taxi_driver_status = models.CharField(verbose_name='Статус таксиста', max_length=250)
+    taxi_driver_coordinates = models.TextField(verbose_name='Координати таксиста')
+
+    def __str__(self):
+        return 'Координати таксистів'
+
+    class Meta:
+        verbose_name = 'Координати таксиста'
+        verbose_name_plural = 'Координати таксистів'
