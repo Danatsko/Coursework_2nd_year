8cf2c07037bb0685e50faaa8736d63d0f5899ca5
Danatsko <oleksandrdanatsko@gmail.com>
2024-05-25 01:47:21 +0300

    Оновив файл `server\settings.py`. Дозволив всі хости.

--- server/settings.py
+++ server/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-312e(=hfiicfdcwfjj+m$nw(d8sk4bzfdiux6wxt0ensoyq_&m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
